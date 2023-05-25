564160f6e273118c8488a4e4a9e9c30bf92e3c0f
rahulkotla2 <rahulkotla2@gmail.com>
2023-05-25 20:31:35 +0530

    Moved some Reactive Data to Global state

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <template>
-  <h2 v-if="isLoading">Loading.....</h2>
+  <base-loader v-if="isLoading"></base-loader>
   <auth-component v-else-if="!isAutherized"></auth-component>
   <list-todo v-else></list-todo>
-  <button @click="signOut">Sign Out</button>
-  
+
   <!-- <Authenticator>
     <template v-slot="{ user, signOut }">
       <h1>Hello {{ user.username }}!</h1>
@@ -17,82 +16,45 @@
 import { Auth } from 'aws-amplify';
 // import "@aws-amplify/ui-vue/styles.css";
 import AuthComponent from './Components/Auth/AuthComponent.vue';
-import {useTodoStore} from './store/TodoStore.js';
+import { useTodoStore } from './store/TodoStore.js';
 import ListTodo from './Components/TodoCRUD/ListTodo.vue';
+import BaseLoader from './Components/BaseLoader.vue'
+import { useAuthentication } from './store/Auth.js';
 export default {
   components: {
     // Authenticator,
     AuthComponent,
     ListTodo,
+    BaseLoader
   },
   data() {
     return {
-      isAutherized: false,
-      isLoading: false,
-      TodoStore : useTodoStore()
+      AuthStore: useAuthentication(),
+      TodoStore: useTodoStore()
     }
   },
   methods: {
-  //   async signIn() {
-  //     const user = await Auth.signIn(this.username, this.password);
-  //     console.log(user);
-  //   },
     async signOut() {
       const user = await Auth.signOut();
       console.log(user);
     },
-  //   async signUp() {
-  //     const username = this.username;
-  //     const password = this.password;
-  //     const email = this.email;
-  //     try {
-  //       const { user } = await Auth.signUp({
-  //         username,
-  //         password,
-  //         attributes: {
-  //           email,
-  //         },
-  //         autoSignIn: { // optional - enables auto sign in after user is confirmed
-  //           enabled: true,
-  //         }
-  //       });
-  //       console.log(user);
-  //     } catch (error) {
-  //       console.log('error signing up:', error);
-  //     }
-  //   },
-  //   async confirmSignUp() {
-  //     try {
-  //       const result = await Auth.confirmSignUp(this.username,this.code,{
-  //         autoSignIn: {
-  //           enabled: true,
-  //         }
-  //       });
-  //       console.log(result);
-  //     } catch (error) {
-  //       console.log('error confirming sign up', error);
-  //     }
-  //   }
   },
-  
+  computed: {
+    isAutherized() {
+      console.log(this.AuthStore.isAuthenticated);
+      return this.AuthStore.isAuthenticated;
+    },
+    isLoading(){
+      return this.AuthStore.isLoading;
+    }
+  },
   mounted() {
     console.log(this.TodoStore.getTodos);
   },
   async created() {
-    console.log(Auth);
-    try {
-      this.isLoading = true;
-      const userData = await Auth.currentUserInfo();
-      if(Object.keys(userData).length){
-        this.isAutherized = true;
-        console.log(Object.keys(userData))
-      }
-      this.isLoading = false;
-      console.log(userData);
-    } catch (e) {
-      this.isAutherized = false;
-      this.isLoading= false;
-    }
+    this.isLoading = true;
+    await this.AuthStore.checkAuthentication();
+    this.isLoading = false;
   }
 }
 </script>
