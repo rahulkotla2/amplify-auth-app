a1f8153e56e9755af70f9c6493f1ed75069ae0e5
rahulkotla2 <rahulkotla2@gmail.com>
2023-05-27 20:13:05 +0530

    Updated Todo Store for Retrieving Data and Storing in Global State

--- src/store/TodoStore.js
+++ src/store/TodoStore.js
@@ -3,22 +3,17 @@ import { defineStore } from 'pinia';
 export const useTodoStore = defineStore('todoStore',{
     state(){
         return {
-            todos: [
-                {
-                    id: '1',
-                    todo: 'Complete Work on Pinia',
-                    description: 'Read Pinia Documentation'
-                },
-                {
-                    id: '2',
-                    todo: 'Complete Work on Amplify',
-                    description: 'Read Amplify Documentation and Practice Auth'
-                }]
+            todos: [],
         }
     },
     getters : {
         getTodos(state){
             return state.todos;
+        }
+    },
+    actions : {
+        setTodos(items){
+           this.todos = items;
         }
     }
 })
