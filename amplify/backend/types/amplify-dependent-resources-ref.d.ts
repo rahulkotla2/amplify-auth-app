20c46712e868cba72b126b21aa3ceabce615089d
rahulkotla2 <rahulkotla2@gmail.com>
2023-05-27 20:08:41 +0530

    Added Amplify Auth and Updated the GraphQL Schema in Amplify API

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,10 +1,19 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyauthapp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "amplifyauthapp4d491d3b": {
       "AppClientID": "string",
       "AppClientIDWeb": "string",
+      "HostedUIDomain": "string",
       "IdentityPoolId": "string",
       "IdentityPoolName": "string",
+      "OAuthMetadata": "string",
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
