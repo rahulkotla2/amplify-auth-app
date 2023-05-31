f9f61d8a71da2bf3e35bad3f6e28e120d6264c18
rahulkotla2 <rahulkotla2@gmail.com>
2023-05-31 10:19:18 +0530

    Optimized the main.js

--- src/main.js
+++ src/main.js
@@ -1,20 +1,21 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import { createPinia } from 'pinia';
-// import '@fortawesome/fontawesome-free/css/all.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faGoogle } from '@fortawesome/free-brands-svg-icons'
 import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import VerifyModel from './Components/TodoCRUD/VerifyModel.vue'
 library.add(faUserSecret,faGoogle)
 
 import { Amplify } from 'aws-amplify';
-import awsconfig from './aws-exports';
+import awsconfig from './awsUpdateConfig';
 
 Amplify.configure(awsconfig);
 
 const app = createApp(App)
 // app.useComponent('font-awesome-icon', FontAwesomeIcon)
-app.component('font-awesome-icon', FontAwesomeIcon)
+app.component('font-awesome-icon', FontAwesomeIcon);
+app.component('verify-model', VerifyModel);
 app.use(createPinia())
 app.mount('#app')
